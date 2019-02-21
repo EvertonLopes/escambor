ce0a57ba9e9a9d3620ed999feb9ccb3c03804b39
Everton L. Costa <everton.lopess@outlook.com>
2019-02-21 09:32:31 -0300

    Ajustando identacao

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  namespace :site do
-    get 'home', to: 'home#index'
-  end
-  namespace :backoffice do
-    get 'dashboard', to: 'dashboard#index'
-  end
+    namespace :site do
+        get 'home', to: 'home#index'
+    end
+    namespace :backoffice do
+        get 'dashboard', to: 'dashboard#index'
+    end
 
-  devise_for :admins
-  devise_for :members
-  
-  root 'site/home#index'
+    devise_for :admins
+    devise_for :members
+    
+    root 'site/home#index'
 end
