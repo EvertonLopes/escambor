4f6d9ceb82a51dfcea2345596d32afb94d7a9884
Everton L. Costa <everton.lopess@outlook.com>
2019-03-11 17:11:17 -0300

    Scope: usando modo de pesquisa junto com lambda em Admin.rb

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,10 +3,12 @@
 class Admin < ApplicationRecord
   enum role: %i[full_access restrict_access]
 
+  scope :with_full_access, -> { where(role: 'full_access') }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
 
   def role_br
     if role == 'full_access'
